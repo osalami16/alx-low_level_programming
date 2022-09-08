d1090b9b9934ae69a20675d14a80febc135673e1
omolola taiwo <omololataiwo93@gmail.com>
2022-09-08 16:40:48 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/''
-main - print the string in the put function
+/**
+* main - print the string in the put function
 
 Description: using the main function 
 this program prints "programimg  is like building a multillingual puzzle
-Return: 0
+ *  Return: 0
 /
 int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
+  }
