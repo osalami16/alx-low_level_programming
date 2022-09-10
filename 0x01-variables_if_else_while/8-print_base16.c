a43e918a381d2db5765dd6ea2e6a7ac086682eab
omolola taiwo <omololataiwo93@gmail.com>
2022-09-10 08:43:00 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Description: prints numbers to base 16
+ *
+ *
+ *Return: Always 0 (success)
+ *
+ */
+int main(void)
+
+{
+	int num;
+
+	char ch;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++
+			}
+
+			putchar(ch);
+
+
+			putchar('\n');
+			return (0);
+
+}
