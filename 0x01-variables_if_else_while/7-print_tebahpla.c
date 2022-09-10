9e37cfb85e06baa54a07dd99e57b490f02e3519d
omolola taiwo <omololataiwo93@gmail.com>
2022-09-10 08:07:12 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print lower case letter in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+
+	char ch;
+
+	for (ch = 'z'; ch  >= 'a'; ch--)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
