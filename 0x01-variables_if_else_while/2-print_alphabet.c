bd0e04edb82257a555615ae5c38a260b6be89ca2
omolola taiwo <omololataiwo93@gmail.com>
2022-09-09 22:09:30 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 
+
+
 /**
+
  * main - entry point
- * 
- * Description: The scripts lists alphabets in lowercase
+
  *
+
+ * Description: The scripts lists alphabets in lowercase
+
+ *
+
  * Return: Always 0 (Success)
+
  */
+
+
+
+int main(void)
+
+{
+
+	char ch;
+
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+
+
+		putchar(ch);
+
+
+
+	putchar ('\n');
+
+
+
+	return (0);
+
