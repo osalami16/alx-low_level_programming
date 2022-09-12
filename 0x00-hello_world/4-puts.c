99d9ad338e5ef898f1303bd880455c3d56f63973
omolola taiwo <omololataiwo93@gmail.com>
2022-09-11 23:51:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 /**
+ * main - entry point
  *
- *  *main- Serves as the entry point for the program
+ * Return: Always 0 (success)
  *
- *   *
- *
- *Return: By default returns zero if no erros otherwise returns non-zero value     */
+ */
 
 int main(void)
-
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
-
