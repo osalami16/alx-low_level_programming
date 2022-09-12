5db27245fd7adc264ec36d6db58375aad854fe3c
omolola taiwo <omololataiwo93@gmail.com>
2022-09-11 23:12:15 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  *main - entry point
  *
  *Description:print last digit in random number
- * 
+ *
  *Return: Always 0 (success)
  */
 
@@ -18,16 +18,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	 
-	lastdigit = n % 10;
+	lastD = n % 10;
 
+	if (lastD > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
+	else if (lastD == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastD);
+	else if (lastD < 6 && lastD != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD);
 
-	if (last > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n. lastdigit);
-	else if (lastdigit ==  0)
-	printf("las digit of %d is %d and is o or 0\n", n, last)
-	else if (last < 6 && last != 0)
-	olkmprint ("lastdigit of %d is %d and is less than 6 and not 0\n', n, lastdigit);
-
-	return(0);
+	return (0);
 }
