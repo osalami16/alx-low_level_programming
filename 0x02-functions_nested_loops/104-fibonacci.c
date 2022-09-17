6d5bfc67130db006b851738bbd39c41939828d71
omolola taiwo <omololataiwo93@gmail.com>
2022-09-17 11:53:55 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
-
 /**
-*main - entry point
-*Description: prints out the first 98 fibonacci numbers
-*				starting with 1 and 2 then followed by a new line
-*
-*Disclaimer: solution was copied from @Vanessa28
-*Dammnnn super smart!!!
-*
-*Return: Always 0
+* main - print the first 98 fibonacci numbers.
+* Return: Nothing.
 */
 int main(void)
 {
-
 int count;
-unsigned long fib1 = 0, fib2 = 1, sum, half1, half2, fib1_half1, fib1_half2,
-fib2_half1, fib2_half2;
+unsigned long i, j, k;
+unsigned long m, n, p, carry;
 
-for (count = 0; count < 92; count++)
+count = 0;
+i = 0;
+j = 1;
+for (count = 1; count <= 91; count++)
 {
-sum = fib1 + fib2;
-printf("%lu, ", sum);
-
-fib1 = fib2;
-fib2 = sum;
+k = i + j;
+i = j;
+j = k;
+printf("%lu, ", k);
 }
-fib1_half1 = fib1 / 10000000000;
-fib1_half2 = fib1 % 10000000000;
-fib2_half1 = fib2 / 10000000000;
-fib2_half2 = fib2 % 10000000000;
-
-for (count = 93; count < 99; count++)
+m = i % 1000;
+i = i / 1000;
+n = j % 1000;
+j = j / 1000;
+while (count <= 98)
 {
-half1 = fib1_half1 + fib2_half1;
-half2 = fib1_half2 + fib2_half2;
-if (half2 > 9999999999)
-{
-half1 += 1;
-half2 %= 10000000000;
-
-}
-printf("%lu%lu", half1, half2);
-if (count != 98
+carry = (m + n) / 1000;
+p = (m + n) - carry * 1000;
+k = (i + j) + carry;
+m = n;
+n = p;
+i = j;
+j = k;
+if (p >= 100)
+printf("%lu%lu", k, p);
+else
+printf("%lu0%lu", k, p);
+if (count != 98)
 printf(", ");
-
-fib1_half1 = fib2_half1;
-fib1_half2 = fib2_half2;
-fib2_half1 = half1;
-fib2_half2 = half2;
-}
-printf("\n");
+count++;
+{}
+putchar('\n');
 return (0);
 }
