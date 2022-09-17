4c6e7ea3b0115162174deb68c9e60a113d0b66f2
omolola taiwo <omololataiwo93@gmail.com>
2022-09-17 05:36:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - entry point
- * 
- * Description: lists lowercase alphabets except  e and q
- *
- * Return: Always 0 (succes)
- */
-
+*main - print if the number is postive, zero, or negative
+*
+*Description: using the main function
+*this program prints "Programming is positive, zero, or negative
+*Return: 0
+*/
 int main(void)
- {
-      char ch;
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
