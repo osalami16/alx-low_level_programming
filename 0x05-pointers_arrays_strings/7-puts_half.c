1c662b5a41aa5bd06adc2a0dc3d7d59ee08c993c
omolola taiwo <omololataiwo93@gmail.com>
2022-09-19 13:03:58 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* puts_half - print half array
+* @str: string
+#/
+void puts_half(char *str)
+{
+int a = 0;
+int b = 0;
+
+while (str[a] != '\0')
+{
+a++;
+}
+a++;
+if (a % 2 == 0)
+{
+b = a / 2;
+}
+else 
+{
+b = (a - 1) / 2;
+}
+while (str[b] != '\0')
+{
+_putchar(str[b]);
+b++;
+}
+_putchar('\n');
+}
