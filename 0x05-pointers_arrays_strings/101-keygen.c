0991ed816657dc7115be475f269371f801ee8bac
omolola taiwo <omololataiwo93@gmail.com>
2022-09-20 16:19:40 -0700

    pointers array

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,42 +1,28 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include "main.h"
 
 /**
-*main - random password generator for 101-crackme
-*
-*Return: always 0
-*/
+ * main - entry point
+ *
+ * Return: generated password
+ *
+ */
+
 int main(void)
 {
-int i, j, k, s;
-char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char p[58];
+	char c;
+	int x;
 
-srand(time(NULL));
-while (s != 2772)
-{
-i = k = s = 0;[D
-while ((2772 - 122) > s)
-{
-j = rand() % 62;
-p[i] = c[j];
-s += c[j];
-i++;
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		x += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+	return (0);
 }
-while (c[k])
-{
-if (c[k] == (2772 - s))
-{
-p[i] = c[k];
-s += c[k];
-i++;
-break;
-}
-k++;
-}
-}
-p[i] = '\0';
-printf("%s", p);
-return (0);
-}
+
