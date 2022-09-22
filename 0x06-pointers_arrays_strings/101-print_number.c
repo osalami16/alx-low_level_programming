97342524fa3a81c1411b3705307bb8f3191d57c3
omolola taiwo <omololataiwo93@gmail.com>
2022-09-21 21:49:04 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,27 @@
 #include "main.h"
+
 /**
-* print_number - prints an integer
-*@n:.input integer paramet
-*
-*/
+ * print_number - recursive print a number
+ * digit by digit
+ * @n: integer to print
+ * Return: void
+ */
 void print_number(int n)
 {
-unsigned int i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
+	if (n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		print_number(n / 10);
+
+		_putchar('0' + (n % 10));
+	}
+}
