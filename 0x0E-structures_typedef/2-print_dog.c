6b059a8fe5384e3648b8185f8887a0bc0a99c5e2
omolola taiwo <omololataiwo93@gmail.com>
2022-10-10 12:49:40 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_dog - prints dog data
+ * @d: dog struct
  */
-int main(void)
-{
-	struct dog my_dog;
 
-	my_dog.name = "Django";
-	my_dog.age = 3.5;
-	my_dog.owner = "Jay";
-	print_dog(&my_dog);
-	return (0);
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+				(*d).name != NULL ? d->name : "(nil)",
+				(*d).age,
+				(*d).owner != NULL ? d->owner : "(nil)");
+	}
 }
