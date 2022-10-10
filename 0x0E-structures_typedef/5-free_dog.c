653b9af787916d200956589bf29297d4f46bfe96
omolola taiwo <omololataiwo93@gmail.com>
2022-10-10 16:25:56 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,15 +2,19 @@
 #include "dog.h"
 
 /**
- * free_dog - frees dogs
- * @d: a dog
+ * free_dog - free struct pointer
+ *
+ * @d: pointer to free
+ *
  * Return: nothing
  */
+
 void free_dog(dog_t *d)
 {
-	if (d == N)
-		return;
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
