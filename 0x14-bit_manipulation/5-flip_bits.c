3c9b9267255a678a1795bc96e6304e2ca7f8d48a
omolola taiwo <omololataiwo93@gmail.com>
2022-11-02 08:02:24 -0700

    a function that returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,12 +2,12 @@
 
 /**
  * flip_bits - returns the number of bits you would need to flip to get
- * 		from one number to another
- * n: a denary number
+ * from one number to another
+ * @n: a denary number
  * @m: another denary number
  * Return: an integer
  */
-uint flip_bits(ulong n, ulong m)
+ uint flip_bits(ulong n, ulong m)
 {
 	ulong o;
 	uint i = 0;
@@ -22,3 +22,4 @@ uint flip_bits(ulong n, ulong m)
 	}
 	return (i);
 }
+ 
